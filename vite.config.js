63559014c0666df775532b68f95b7721cb9a1b83
ShlokDeota06 <deotamanisha2006@gmail.com>
2025-09-10 23:04:16 +0530

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,8 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// Replace `your-username` with your actual GitHub username
+// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/pashu/' 
+  base: '/cattle-breed-recognition/'
 })
